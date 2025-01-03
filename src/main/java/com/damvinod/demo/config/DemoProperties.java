8460486870c4acab108c0ab36234e9571439bcd7
Vinod Dampuru <vinodreddyreddy25@gmail.com>
2025-01-03 12:02:52 +0800

    Correcting the variable for docker example service

--- src/main/java/com/damvinod/demo/config/DemoProperties.java
+++ src/main/java/com/damvinod/demo/config/DemoProperties.java
@@ -2,6 +2,6 @@ package com.damvinod.demo.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "demo.service")
+@ConfigurationProperties(prefix = "docker.example")
 public record DemoProperties(String host, String getHelloResponse) {
 }
